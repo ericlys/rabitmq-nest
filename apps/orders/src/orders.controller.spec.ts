3777f60c71ab185ab0ae014c1f610b9b6060436d
Ericlys Moreira <ericlysm@gmail.com>
2024-11-21 06:04:23 -0300

    feat: add register order

--- apps/orders/src/orders.controller.spec.ts
+++ apps/orders/src/orders.controller.spec.ts
@@ -1,22 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { OrdersController } from './orders.controller';
-import { OrdersService } from './orders.service';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { OrdersController } from './orders.controller';
+// import { OrdersService } from './orders.service';
 
 describe('OrdersController', () => {
-  let ordersController: OrdersController;
-
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [OrdersController],
-      providers: [OrdersService],
-    }).compile();
-
-    ordersController = app.get<OrdersController>(OrdersController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(ordersController.getHello()).toBe('Hello World!');
-    });
-  });
+  // let ordersController: OrdersController;
+  // beforeEach(async () => {
+  //   const app: TestingModule = await Test.createTestingModule({
+  //     controllers: [OrdersController],
+  //     providers: [OrdersService],
+  //   }).compile();
+  //   ordersController = app.get<OrdersController>(OrdersController);
+  // });
+  // describe('root', () => {
+  //   it('should return "Hello World!"', () => {
+  //     expect(ordersController.getHello()).toBe('Hello World!');
+  //   });
+  // });
 });
